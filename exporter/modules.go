3b5ccb6fc0d278be30e5e5800d562c9ce5660f7e
Cargo Audit <zimber.dudi@gmail.com>
2026-05-10 10:45:44 +0300

    fix: improve line splitting and trimming in parseModulesInfo function

--- exporter/modules.go
+++ exporter/modules.go
@@ -27,14 +27,16 @@ func (e *Exporter) extractModulesMetrics(ch chan<- prometheus.Metric, c redis.Co
 }
 
 func (e *Exporter) parseModulesInfo(ch chan<- prometheus.Metric, info string) {
-	lines := strings.SplitSeq(info, "\r\n")
-	for line := range lines {
+	lines := strings.Split(info, "\n")
+	for _, line := range lines {
+		line = strings.TrimSpace(line)
 		log.Debugf("info: %s", line)
 
-		split := strings.Split(line, ":")
-		if len(split) != 2 {
+		if len(line) < 2 || !strings.Contains(line, ":") {
 			continue
 		}
+
+		split := strings.SplitN(line, ":", 2)
 
 		if split[0] == "module" {
 			// module format: 'module:name=<module-name>,ver=21005,api=1,filters=0,usedby=[],using=[],options=[]'
